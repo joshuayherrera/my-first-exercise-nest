3e8b2237ce5d7b554efed81c534ab0be099bc715
Joshua Alvarez Herrera <joshuayherrera@gmail.com>
2025-05-19 10:04:40 -0500

    feat: :sparkles: Pipe Validation Implemented
    Now the DTO have their own validation. Of course, the configuration it is in main.ts

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,14 @@
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+    @IsString({ message: 'The title must be a string' })
+    @IsNotEmpty({ message: 'The title is required' })
+    @MinLength(3, { message: 'The title must be at least 3 characters long' })
+    @MaxLength(50, { message: 'The title must be at most 50 characters long' })
     title: string;
+
+    @IsString({ message: 'The description must be a string' })
+    @IsOptional()
+    @MaxLength(255)
     description?: string;
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,18 @@
+import { IsBoolean, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
 export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(50)
   title?: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   isDone?: boolean;
 }
